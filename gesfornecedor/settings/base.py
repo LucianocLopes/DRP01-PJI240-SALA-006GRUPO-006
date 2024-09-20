0101c4c7e3d14121086a023e310bad6433b47777
LucianoCLopes <luciano_clopes@hotmail.com>
2024-09-20 01:01:45 -0300

    FEAT - Config basic and corrections app Core

--- gesfornecedor/settings/base.py
+++ gesfornecedor/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     # 3td Apps
     # MY Apps
+    'core',
 ]
 
 MIDDLEWARE = [
--- gesfornecedor/urls.py
+++ gesfornecedor/urls.py
@@ -3,11 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.generic import TemplateView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="base.html"), name='index'),
+    path("", include('core.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
