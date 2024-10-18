440f74dd2c8797be3d6b6dbf2cc4bec571ac80b6
Rosana TSF <rosanatsf@gmail.com>
2024-10-18 14:40:43 -0300

    Add about e contact ao template

--- suppli_request/views.py
+++ suppli_request/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView, CreateView, DetailView, ListView
 from django.http import HttpResponseRedirect
@@ -18,21 +18,23 @@ class SupplyResquestListView(ListView):
 class SupplyRequestCreateView(CreateView):
     model = models.SupplyResquest
     template_name = "suppli_request/supplyrequest/CRUD/create.html"
-    
-    
-    fields = ["date_request",
-                "description_request",
-                "status",
-            ]
-    
+    fields = ["date_request", "description_request", "status"]
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.created_by = self.request.user
         self.object.save()
-        
         return HttpResponseRedirect(self.get_success_url())
-    
+
 
 class SupplyRequestDetailView(DetailView):
     model = models.SupplyResquest
     template_name = "suppli_request/supplyrequest/CRUD/detail.html"
+
+
+# Views para Contato e Sobre
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
